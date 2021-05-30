7b927882cb049222715e0da9c4232d1295679d81
Francisco Cachon <fracachon@gmail.com>
2021-05-30 16:48:41 -0300

    Se agregó read

---
+++ src/Read.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Read {
+
+    public static void main(String args[]) {
+        String pathJuan = "";
+        String pathFran = "";
+        String line = "";
+
+        try {
+            BufferedReader br = new BufferedReader( new FileReader(pathFran));
+            while(line = br.readLine() != null) {
+                String[] values = line.split(",");
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
