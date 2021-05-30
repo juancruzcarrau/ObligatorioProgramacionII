d819e0e56c62b32d5028ffb982711d14b30f0c8d
Juan Cruz <juancruzcarrau1@gmail.com>
2021-05-30 19:30:47 -0300

    Se creo el menu.

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("Seleccione la opción que desee:");
+            System.out.println("\t1. Carga de datos");
+            System.out.println("\t2. Ejecutar consultas");
+            System.out.println("\t3. Salir");
+            System.out.print("Seleccion: ");
+
+            int seleccion;
+
+            try{
+                seleccion = scanner.nextInt();
+            } catch (InputMismatchException e){
+                System.out.println("Ingrese un numero. Intente de nuevo.");
+                scanner.next();
+                continue;
+            }
+
+            if(!Arrays.asList(1, 2, 3).contains(seleccion)){
+                System.out.println("Seleccion incorrecta. Intente de nuevo.");
+                continue;
+            }
+
+            if(seleccion == 1){
+//                cargarDatos();
+            } else if (seleccion == 2){
+//                ejectutarConsultas();
+            } else if (seleccion == 3){
+                break;
+            } else {
+                throw new RuntimeException("Ha ocurrido un error. Seleccion no puede ser un nro distinto de 1, 2 o 3.");
+            }
+        }
+    }
+}
